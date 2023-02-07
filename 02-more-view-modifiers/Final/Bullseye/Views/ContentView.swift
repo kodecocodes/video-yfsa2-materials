891d6538e627ba4f9867f070faa98e25567a0e70
Catie <catiet@gmail.com>
2023-02-07 13:03:43 -0500

    Recording prep

---
+++ 02-more-view-modifiers/Final/Bullseye/Views/ContentView.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct ContentView: View {
+  @State private var alertIsVisible = false
+  @State private var sliderValue = 50.0
+  @State private var game = Game()
+
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+        .padding(.horizontal, 30)
+      Text(String(game.target))
+        .kerning(-1.0)
+        .font(.largeTitle)
+        .fontWeight(.black)
+      HStack {
+        Text("1")
+          .bold()
+        Slider(value: $sliderValue, in: 1.0...100.0)
+        Text("100")
+          .bold()
+      }
+      .padding()
+      Button("Hit me".uppercased()) {
+        alertIsVisible = true
+      }
+      .padding(.all)
+      .background(Color.blue)
+      .foregroundColor(.white)
+      .cornerRadius(21.0)
+      .bold()
+      .font(.title3)
+      .alert(
+        "Hello there!",
+        isPresented: $alertIsVisible,
+        actions: {
+          Button("Awesome") {
+            print("Alert closed")
+          }
+        },
+        message: {
+          let roundedValue = Int(sliderValue.rounded())
+          Text("""
+            The slider's value is \(roundedValue).
+            You scored \(game.points(sliderValue: roundedValue)) points this round.
+          """)
+        }
+      )
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
